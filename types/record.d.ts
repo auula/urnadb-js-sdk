0836d3aba4278b09e4a7bb24caa7b13afb795fbe
Leon Ding <ding_ms@outlook.com>
2026-07-26 17:01:02 +0800

    feat: add support type record api.

--- types/record.d.ts
+++ types/record.d.ts
@@ -7,5 +7,7 @@ export default class Record {
     get(key: string): any;
     set(key: string, value: any): void;
     has(key: string): boolean;
+    query(key:string):any;
+    search(key:string):any;
     build(): Record<string, any>;
 }
